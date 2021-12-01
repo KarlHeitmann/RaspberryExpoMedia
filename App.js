9ef18ab6f996aef8089ea99365d42969f109c7c8
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-30 22:20:30 -0300

    aplicación tiene la funcionalidad de toggle, y muestra artist y title

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isHungry, setIsHungry] = useState("NADA");
+  const [artist, setArtist] = useState("Sin artista");
+  const [title, setTitle] = useState("Sin titulo");
+  const [status, setStatus] = useState("Sin titulo");
+
+  const statusFunc = (e) => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HOLA </Text>
+      <Text>{artist}</Text>
+      <Text>{title}</Text>
+      <Text>HOLA Chao</Text>
+      <Button
+        onPress={async () => {
+          const resp = await fetch('http://192.168.8.110:4567')
+          const names = await resp.json();
+          setIsHungry(`Artista: ${names.artist}`)
+          setArtist(names.artist)
+          setTitle(names.title)
+          setStatus(names.status)
+          console.log(names)
+          //setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title="Status"
+      />
+      <Text>{status}</Text>
+      <Button
+        onPress={async () => {
+          await fetch('http://192.168.8.110:4567/toggle')
+          //setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={status == "playing" ? "PAUSE" : "Play"}
+      />
       <StatusBar style="auto" />
     </View>
   );
