290e7d7356e3b709efde96b5c0cc43ddf6da3fc6
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-19 00:37:49 -0300

    permite agregar una pista desde browse al playlist

--- components/browse.js
+++ components/browse.js
@@ -6,31 +6,46 @@ const DOMAIN_API = 'http://192.168.8.117:4567'
 export default function Browse() {
   const [files, setFiles] = useState([])
 
+  const fetchFiles = async(f) => {
+    console.log("bla bla bla")
+    const query_params = f == '' ? '' : `?folder=${f}`
+    console.log(query_params)
+    const resp = await fetch(`${DOMAIN_API}/ls${query_params}`)
+    console.log(resp)
+    const files_tmp = await resp.json();
+    console.log(files_tmp)
+    return files_tmp
+  }
+
   const updateFiles = async(f) => {
-    const resp = await fetch(`${DOMAIN_API}/ls?folder=${f}`)
-    const files = await resp.json();
-    setFiles(files)
+    setFiles(fetchFiles(f))
+
+  }
+
+  const addFile = async(f) => {
+    const resp = await fetch(`${DOMAIN_API}/add?file=${f}`)
+    const files_tmp = await resp.json();
 
   }
 
   useEffect(async() => {
     console.log("Browse")
-    const resp = await fetch(`${DOMAIN_API}/ls`)
-    const files = await resp.json();
-    setFiles(files)
+    //setFiles(fetchFiles(''))
   }, [])
   // console.log(songs)
   return (
     <View>
       <Text>Browse</Text>
+      <Button onPress={async (e)=>{console.log("click!");setFiles(await fetchFiles(''))}} title='Root'/>
       {
         files.map(function(file, i) {
           return <Text key={i}>
             {
               file.includes('.mp3') ? 
-              <Text>file</Text> : <Button onPress={(e) => {updateFiles(file)}} title={file} />
+              <Button onPress={async(e) => {addFile(file)}} title={`Add ${file.split('/')[file.split('/').length - 1]}`}/> : <Button onPress={async(e) => {setFiles(await fetchFiles(file))}} title={file} />
             }
             --- {file}
+
           </Text>
         })
       }
