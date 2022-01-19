c86a44525d84c59efc0d5a7998f0a5e8b1175591
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-18 23:33:21 -0300

    colocado navegacion basica

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import CurrentData from './components/current_data';
-import Playlist from './components/playlist';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
 import Main from './screens/main';
+
 
 const DOMAIN_API = 'http://192.168.8.117:4567'
 
+function About({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>About Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Main')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
+
+    // <Main />
