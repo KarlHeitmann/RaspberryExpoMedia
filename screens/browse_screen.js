16e82bc232e9a051a9feaaf4e89ad7ff76b0f06d
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-20 00:11:10 -0300

    added scroll to browse screen

--- screens/browse_screen.js
+++ screens/browse_screen.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Browse from '../components/browse';
-import { Button, Text, View } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function BrowseScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Browse</Text>
-      <Browse />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Browse</Text>
+          <ScrollView style={styles.scrollView}>
+            <Browse />
+          </ScrollView>
+        </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+  },
+  negrita: {
+    fontWeight: "700"
+  },
+  normal: {
+    fontWeight: "100"
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+
+    marginHorizontal: 1,
+  },
+  text: {
+    fontSize: 42,
+  },
+});
+
+
